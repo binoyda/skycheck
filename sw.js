6ecefbb7a74081e563fe22f3bb02781659cb4642
binoyda <binoyda@gmail.com>
2026-09-02 09:01:16 -0500

    Add air quality (US AQI) and visibility tiles

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // SkyCheck service worker: cache the app shell, always go to the network for weather data.
-const CACHE = 'skycheck-v7';
+const CACHE = 'skycheck-v8';
 const SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
