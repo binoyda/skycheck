c344eb69572195d445f903f43a053c5b65b1f029
binoyda <binoyda@gmail.com>
2026-09-02 09:15:06 -0500

    Condition and H/L shown beside the main temperature

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // SkyCheck service worker: cache the app shell, always go to the network for weather data.
-const CACHE = 'skycheck-v9';
+const CACHE = 'skycheck-v10';
 const SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
